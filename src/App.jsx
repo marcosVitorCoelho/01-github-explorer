797bb15f2135b089c485a71513b356890906bcf5
marcosVitorCoelho <marcosvitorcoelho@gmail.com>
2021-09-15 01:49:15 -0300

    alteracao estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
   return (
-    <> {/* fake tag pai que a funcao pede*/}
       <RepositoryList />
-      <Counter />
-    </>
-
   )
 }
